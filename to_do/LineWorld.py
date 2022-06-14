d76db3043ac84275d295d4c9151de59b342ad67b
Syphax Sarni <36453448+SyphaxSarni@users.noreply.github.com>
2022-06-14 11:55:35 +0200

    first version

---
+++ to_do/LineWorld.py
@@ -0,0 +1,93 @@
+import math
+from random import random
+
+from drl_lib.do_not_touch.contracts import *
+
+
+class LineWorldMDP(MDPEnv):
+
+    def __init__(self):
+        self.S = np.array([i for i in range(7)])
+        self.A = np.array([0, 1])
+        self.R = np.array([-1.0, 0.0, 1.0])
+        self.psasr = np.zeros((len(self.S), len(self.A), len(self.S), len(self.R)))
+
+        for s in self.S[1:-1]:
+            if s == 1:
+                self.psasr[s, 0, s - 1, 0] = 1.0
+            else:
+                self.psasr[s, 0, s - 1, 1] = 1.0
+
+            if s == 7 - 2:
+                self.psasr[s, 1, s + 1, 2] = 1.0
+            else:
+                self.psasr[s, 1, s + 1, 1] = 1.0
+
+    def states(self) -> np.ndarray:
+        return self.S
+
+    def actions(self) -> np.ndarray:
+        return self.A
+
+    def rewards(self) -> np.ndarray:
+        return self.R
+
+    def is_state_terminal(self, s: int) -> bool:
+        return s == 0 or s == 6
+
+    def transition_probability(self, s: int, a: int, s_p: int, r: float) -> float:
+        return self.psasr[s, a, s_p, r]
+
+    def view_state(self, s: int):
+        pass
+
+
+class LineWorld(SingleAgentEnv):
+
+    def __init__(self, nb_cells: int = 7):
+        self.nb_cells = nb_cells
+        self.current_cell = math.floor(nb_cells / 2)
+        self.step_count = 0
+
+    def state_id(self) -> int:
+        return self.current_cell
+
+    def is_game_over(self) -> bool:
+        return self.current_cell == self.nb_cells - 1 or self.current_cell == 0
+
+    def act_with_action_id(self, action_id: int):
+        self.step_count += 1
+        if action_id == 0:
+            self.current_cell -= 1
+        else:
+            self.current_cell += 1
+
+    def score(self) -> float:
+        if self.current_cell == 0:
+            return -1.0
+        elif self.current_cell == self.nb_cells - 1:
+            return 1.0
+        else:
+            return 0.0
+
+    def available_actions_ids(self) -> np.ndarray:
+        return np.array([0, 1])
+
+    def reset(self):
+        self.current_cell = math.floor(self.nb_cells / 2)
+        self.step_count = 0
+
+    def view(self):
+        print(f"Game over : {self.is_game_over()}")
+        print(f"Score : {self.score()}")
+        print(f"Number of steps : {self.score()}")
+        for i in range(self.nb_cells):
+            if i == self.current_cell:
+                print("X", end='')
+            else:
+                print("_", end='')
+        print()
+
+    def reset_random(self):
+        self.current_cell = random.randint(0, self.nb_cells - 1)
+        self.step_count = 0
---
+++ to_do/dynamic_programming.py
@@ -0,0 +1,155 @@
+import random as rand
+
+from drl_lib.do_not_touch.mdp_env_wrapper import Env1
+from drl_lib.do_not_touch.result_structures import ValueFunction, PolicyAndValueFunction
+from LineWorld import *
+
+
+def policy_evaluation_on_line_world() -> ValueFunction:
+    """
+    Creates a Line World of 7 cells (leftmost and rightmost are terminal, with -1 and 1 reward respectively)
+    Launches a Policy Evaluation Algorithm in order to find the Value Function of a uniform random policy
+    Returns the Value function (V(s)) of this policy
+    """
+    # Création de l'environnement
+    mdp_env = LineWorldMDP()
+
+    # Création de la policy
+    policy = {}
+    for s in mdp_env.states():
+        actions = {}
+        for j in mdp_env.actions():
+            actions[j] = 0.5
+        policy[s] = actions
+    policy[0] = {0: 0.0, 1: 0.0}
+    policy[6] = {0: 0.0, 1: 0.0}
+
+    # Algorithme
+    threshold = 0.00001
+
+    # Création et intialisation de la value function
+    value_function = {}
+    for s in mdp_env.states():
+        value_function[s] = rand.random()
+    value_function[0] = 0.0
+    value_function[6] = 0.0
+
+    while True:
+        delta = 0
+        S = mdp_env.states()
+
+        for s in S:
+            v = value_function[s]
+            value_function[s] = 0.0
+            A = mdp_env.actions()
+            for a in A:
+                total = 0.0
+                for s_p in S:
+                    R = mdp_env.rewards()
+                    for r in range(len(R)):
+                        total += mdp_env.transition_probability(s, a, s_p, r) * (R[r] + 0.999 * value_function[s_p])
+                    total *= policy[s][a]
+                    value_function[s] += total
+                delta = max(delta, np.abs(v - value_function[s]))
+        if delta < threshold:
+            break
+    return value_function
+
+def policy_iteration_on_line_world() -> PolicyAndValueFunction:
+    """
+    Creates a Line World of 7 cells (leftmost and rightmost are terminal, with -1 and 1 reward respectively)
+    Launches a Policy Iteration Algorithm in order to find the Optimal Policy and its Value Function
+    Returns the Policy (Pi(s,a)) and its Value Function (V(s))
+    """
+
+    # TODO
+    pass
+
+
+def value_iteration_on_line_world() -> PolicyAndValueFunction:
+    """
+    Creates a Line World of 7 cells (leftmost and rightmost are terminal, with -1 and 1 reward respectively)
+    Launches a Value Iteration Algorithm in order to find the Optimal Policy and its Value Function
+    Returns the Policy (Pi(s,a)) and its Value Function (V(s))
+    """
+    # TODO
+    pass
+
+
+def policy_evaluation_on_grid_world() -> ValueFunction:
+    """
+    Creates a Grid World of 5x5 cells (upper rightmost and lower rightmost are terminal, with -1 and 1 reward respectively)
+    Launches a Policy Evaluation Algorithm in order to find the Value Function of a uniform random policy
+    Returns the Value function (V(s)) of this policy
+    """
+    # TODO
+    pass
+
+
+def policy_iteration_on_grid_world() -> PolicyAndValueFunction:
+    """
+    Creates a Grid World of 5x5 cells (upper rightmost and lower rightmost are terminal, with -1 and 1 reward respectively)
+    Launches a Policy Iteration Algorithm in order to find the Optimal Policy and its Value Function
+    Returns the Policy (Pi(s,a)) and its Value Function (V(s))
+    """
+    # TODO
+    pass
+
+
+def value_iteration_on_grid_world() -> PolicyAndValueFunction:
+    """
+    Creates a Grid World of 5x5 cells (upper rightmost and lower rightmost are terminal, with -1 and 1 reward respectively)
+    Launches a Value Iteration Algorithm in order to find the Optimal Policy and its Value Function
+    Returns the Policy (Pi(s,a)) and its Value Function (V(s))
+    """
+    # TODO
+    pass
+
+
+def policy_evaluation_on_secret_env1() -> ValueFunction:
+    """
+    Creates a Secret Env1
+    Launches a Policy Evaluation Algorithm in order to find the Value Function of a uniform random policy
+    Returns the Value function (V(s)) of this policy
+    """
+    env = Env1()
+    # TODO
+    pass
+
+
+def policy_iteration_on_secret_env1() -> PolicyAndValueFunction:
+    """
+    Creates a Secret Env1
+    Launches a Policy Iteration Algorithm in order to find the Optimal Policy and its Value Function
+    Returns the Policy (Pi(s,a)) and its Value Function (V(s))
+    """
+    env = Env1()
+    # TODO
+    pass
+
+
+def value_iteration_on_secret_env1() -> PolicyAndValueFunction:
+    """
+    Creates a Secret Env1
+    Launches a Value Iteration Algorithm in order to find the Optimal Policy and its Value Function
+    Prints the Policy (Pi(s,a)) and its Value Function (V(s))
+    """
+    env = Env1()
+    # TODO
+    pass
+
+
+def demo():
+    print(policy_evaluation_on_line_world())
+    # print(policy_iteration_on_line_world())
+    # print(value_iteration_on_line_world())
+    #
+    # print(policy_evaluation_on_grid_world())
+    # print(policy_iteration_on_grid_world())
+    # print(value_iteration_on_grid_world())
+    #
+    # print(policy_evaluation_on_secret_env1())
+    # print(policy_iteration_on_secret_env1())
+    # print(value_iteration_on_secret_env1())
+
+demo()
