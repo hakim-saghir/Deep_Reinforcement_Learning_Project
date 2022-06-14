304ec875c712748bc47f0a77a62a621064f853f9
Syphax Sarni <36453448+SyphaxSarni@users.noreply.github.com>
2022-06-14 15:23:25 +0200

    Dynamic - Policy Iteration done

--- to_do/dynamic_programming.py
+++ to_do/dynamic_programming.py
@@ -57,6 +57,7 @@ def policy_evaluation_on_line_world() -> ValueFunction:
             break
     return value_function
 
+
 def policy_iteration_on_line_world() -> PolicyAndValueFunction:
     """
     Creates a Line World of 7 cells (leftmost and rightmost are terminal, with -1 and 1 reward respectively)
@@ -64,8 +65,77 @@ def policy_iteration_on_line_world() -> PolicyAndValueFunction:
     Returns the Policy (Pi(s,a)) and its Value Function (V(s))
     """
 
-    # TODO
-    pass
+    # Création de l'environnement
+    mdp_env = LineWorldMDP()
+    S = mdp_env.states()
+    A = mdp_env.actions()
+    R = mdp_env.rewards()
+
+    # Algorithme
+    threshold = 0.0000001
+
+    # Création et intialisation de la value function
+    value_function = {}
+    for s in S:
+        value_function[s] = rand.random()
+    value_function[0] = 0.0
+    value_function[6] = 0.0
+
+    policy = {}
+    for s in mdp_env.states():
+        actions = {}
+        for j in mdp_env.actions():
+            actions[j] = rand.random()
+        policy[s] = actions
+
+    for s in S:
+        for a in A:
+            policy[s][a] /= np.sum(list(policy[s].values()))
+
+    for k in policy[0].keys():
+        policy[0][k] = 0.0
+        policy[6][k] = 0.0
+
+
+    while True:
+        while True:
+            delta = 0
+            for s in S:
+                v = value_function[s]
+                value_function[s] = 0.0
+                A = mdp_env.actions()
+                for a in A:
+                    total = 0.0
+                    for s_p in S:
+                        R = mdp_env.rewards()
+                        for r in range(len(R)):
+                            total += mdp_env.transition_probability(s, a, s_p, r) * (R[r] + 0.999 * value_function[s_p])
+                    total *= policy[s][a]
+                    value_function[s] += total
+                delta = max(delta, np.abs(v - value_function[s]))
+            if delta < threshold:
+                break
+
+        stable = True
+        for s in S:
+            old_policy = policy[s].copy()
+            best_a = -1
+            best_a_score = -9999999
+            for a in A:
+                total = 0
+                for s_p in S:
+                    for r in range(len(R)):
+                        total += mdp_env.transition_probability(s, a, s_p, r) * (R[r] + 0.999 * value_function[s_p])
+                if total > best_a_score:
+                    best_a = a
+                    best_a_score = total
+            for k in policy[s].keys():
+                policy[s][k] = 0.0
+            policy[s][best_a] = 1.0
+            if old_policy != policy[s]:
+                stable = False
+        if stable:
+            return policy, value_function
 
 
 def value_iteration_on_line_world() -> PolicyAndValueFunction:
@@ -142,8 +212,8 @@ def value_iteration_on_secret_env1() -> PolicyAndValueFunction:
 
 
 def demo():
-    print(policy_evaluation_on_line_world())
-    # print(policy_iteration_on_line_world())
+    # print(policy_evaluation_on_line_world())
+    print(policy_iteration_on_line_world())
     # print(value_iteration_on_line_world())
     #
     # print(policy_evaluation_on_grid_world())
@@ -153,5 +223,6 @@ def demo():
     # print(policy_evaluation_on_secret_env1())
     # print(policy_iteration_on_secret_env1())
     # print(value_iteration_on_secret_env1())
+
 
 demo()
